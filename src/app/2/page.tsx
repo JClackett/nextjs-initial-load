b08a385a62973637fb8c3a176116e6411790ac4a
Jack Clackett <jack@noquarter.co>
2025-02-20 11:50:19 +0100

    switch pages

--- src/app/2/page.tsx
+++ src/app/2/page.tsx
@@ -1,22 +1,3 @@
-import { Suspense } from "react"
-import { ClientStuff } from "../client-stuff"
-
-export const dynamic = "force-dynamic"
-
-export default function Home() {
-  return (
-    <>
-      <ClientStuff />
-      <div className="border p-4 rounded">
-        <Suspense fallback={<div>Loading data in 8 seconds...</div>}>
-          <DataFetching />
-        </Suspense>
-      </div>
-    </>
-  )
-}
-
-async function DataFetching() {
-  await new Promise((resolve) => setTimeout(resolve, 8000))
-  return <div>Data Fetched</div>
+export default function Page() {
+  return <div>Page 2 nothing here dynamic</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import { Suspense } from "react"
 import { ClientStuff } from "./client-stuff"
 
-export default function Page() {
+export const dynamic = "force-dynamic"
+
+export default function Home() {
   return (
     <>
       <ClientStuff />
-      <p>Page 1 nothing here dynamic</p>
+      <div className="border p-4 rounded">
+        <Suspense fallback={<div>Loading data in 8 seconds...</div>}>
+          <DataFetching />
+        </Suspense>
+      </div>
     </>
   )
+}
+
+async function DataFetching() {
+  await new Promise((resolve) => setTimeout(resolve, 8000))
+  return <div>Data Fetched</div>
 }
